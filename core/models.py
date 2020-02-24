8a6dca17782223d075400fb933dc5b126acbf160
gdickson52 <gdickson52@gmail.com>
2020-02-24 15:01:58 -0500

    making changes

--- core/models.py
+++ core/models.py
@@ -1,16 +1,21 @@
 from django.db import models
-from datetime import date, time
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 
 # Models for our Habit Tracker.
 
 class Habit(models.Model):
-    name = models.CharField(max_length=200)
-    objective = models.TextField(max_length=700)
+    name = models.CharField(max_length=500, help_text= "Please enter your habit here")
+    objective = models.TextField(max_length=700, help_text= "Please enter the description of your habit here")
+    target = models.IntegerField(help_text= "Please enter a desired target number for your habit")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    observers = models.ManyToManyField(User, related_name='observers')
+    observers = models.ManyToManyField("Observer", blank=True)
     
+    def get_absolute_url(self):
+        return reverse('habit-detail', args=[str(self.id)])
+
     def __str__(self):
         return self.name
 
